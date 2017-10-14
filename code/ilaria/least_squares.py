837740c20fef63de25acdc373c7b1a564a234594
Ilaria Ricchi <ilaria.ricchi@epfl.ch>
2017-10-14 14:30:45 +0200

    modified version i

--- code/ilaria/least_squares.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-
-def least_squares(y, tx):
-    """calculate the least squares."""
-    # ***************************************************
-    return (np.linalg.inv((tx.T).dot(tx))).dot(tx.T).dot(y)
-    # ***************************************************
-    raise NotImplementedError
