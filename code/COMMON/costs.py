f3bbe1aa1bef83343be89a21fb58d5380be1f96d
Tom Mingasson <tommingasson@hotmail.fr>
2017-10-14 16:13:32 +0100

    add costs.py to COMMON

---
+++ code/COMMON/costs.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""Functions used to compute the loss."""
+
+def compute_mse(y, tx, w):
+    """Calculate the loss using mse.
+    """
+   
+    # number of samples
+    N = len(y)
+    
+    # compute the error
+    e = y-tx.dot(w)
+    
+    # when loss is the MSE
+    mse = (1/(2*N))*e.dot(e)
+    
+    return mse
+
+def compute_mae(y, tx, w):
+    """Calculate the loss using mae.
+    """
+   
+    # number of samples
+    N = len(y)
+    
+    # compute the error
+    e = y-tx.dot(w)
+    
+    # when loss is the MAE
+    mae = abs(e).mean()
+    
+    return mae
