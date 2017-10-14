36d4ed780f6ba15a9eabc70b0a4f1b5fb5578015
Flavio Martinelli <flavio.martinelli.95@gmail.com>
2017-10-14 18:21:39 +0200

    working on implementations

--- code/COMMON/implementations.py
+++ code/COMMON/implementations.py
@@ -33,6 +33,31 @@ def compute_gradient(y, tx, w):
 
     return grad
 
+def batch_iter(y, tx, batch_size, num_batches=1, shuffle=True):
+    """
+    Generate a minibatch iterator for a dataset.
+    Takes as input two iterables (here the output desired values 'y' and the input data 'tx')
+    Outputs an iterator which gives mini-batches of `batch_size` matching elements from `y` and `tx`.
+    Data can be randomly shuffled to avoid ordering in the original data messing with the randomness of the minibatches.
+    Example of use :
+    for minibatch_y, minibatch_tx in batch_iter(y, tx, 32):
+        <DO-SOMETHING>
+    """
+    data_size = len(y)
+
+    if shuffle:
+        shuffle_indices = np.random.permutation(np.arange(data_size))
+        shuffled_y = y[shuffle_indices]
+        shuffled_tx = tx[shuffle_indices]
+    else:
+        shuffled_y = y
+        shuffled_tx = tx
+    for batch_num in range(num_batches):
+        start_index = batch_num * batch_size
+        end_index = min((batch_num + 1) * batch_size, data_size)
+        if start_index != end_index:
+            yield shuffled_y[start_index:end_index], shuffled_tx[start_index:end_index]
+
 def least_squares_GD(y, tx, initial_w, max_iters, gamma):
 
     """Gradient descent algorithm."""
@@ -72,7 +97,7 @@ def least_squares_SGD(y, tx, initial_w, max_iters, gamma):
     for n_iter in range(max_iters):
                 
         # pick randomly 'batch_size' samples
-        batches = batch_iter(y, tx, batch_size, num_batches=1, shuffle=True)
+        batches = batch_iter(y, tx, 1, num_batches=1, shuffle=True)
         
         for samples in batches:
 
@@ -93,3 +118,5 @@ def least_squares_SGD(y, tx, initial_w, max_iters, gamma):
     print("Gradient Descent({bi}/{ti}): loss MSE={l}, w0={w0}, w1={w1}".format(bi=n_iter, ti=max_iters - 1, l=loss, w0=w[0], w1=w[1]))
 
     return ws, losses
+
+
