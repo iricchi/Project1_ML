3803544a1d3928096080dabc8c05f83a9ee2ed97
Ilaria Ricchi <ilaria.ricchi@epfl.ch>
2017-10-18 17:21:17 +0200

    Outliers new version

--- code/COMMON/outliers.py
+++ code/COMMON/outliers.py
@@ -1,15 +1,24 @@
 import numpy as np
-#Set delete to 1 if you want to delete 999 from the dataset, if you want to substitude it with the mean of the feature put 0 or any other values
 
+"""
+Set delete to 1 if you want to delete -999 from the dataset,
+Set delete to 0 if you want to substitude -999 with 0,
+Set delete to anyother values if you want to substitude -999 with the mean
+"""
 def handle_outliers(input_data, yb, outlier_value, delete)
     X = input_data
 
-    if delete:
+    if delete==1:
         Y = yb
         for i in range(input_data.shape[1]):
             del_idx = np.where(X[:,i] == outlier_value)
             X = np.delete(X, del_idx, 0)      
             Y = np.delete(Y, del_idx, 0)
+        
+    elif delete==0:
+        for i in range(X.shape[1]):
+            X[np.where(X[:,i]==-999),i] = 0
+
         
     else:
         for i in range(input_data.shape[1]):
