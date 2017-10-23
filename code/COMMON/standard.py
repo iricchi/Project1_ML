15f0ed14a4c2737c4f2f34f1c2c80707c1aed6a6
Ilaria Ricchi <ilaria.ricchi@epfl.ch>
2017-10-23 23:29:58 +0200

    standardization

--- code/COMMON/standard.py
+++ code/COMMON/standard.py
@@ -3,7 +3,7 @@ import numpy as np
 
 def standardize(x):
     """Standardize the original data set."""
-    mean_x = np.mean(x)
+    mean_x = np.mean(x, axis = 0)
     x = x - mean_x
     std_x = np.std(x)
     x = x / std_x
