4ae3c55ddffd6feee812b2a2eedcb467c6c7861b
Ilaria Ricchi <ilaria.ricchi@epfl.ch>
2017-10-23 15:39:06 +0200

    pers

--- code/COMMON/proj1_helpers.py
+++ code/COMMON/proj1_helpers.py
@@ -25,7 +25,7 @@ def load_csv_data(data_path, sub_sample=False):
 def predict_labels(weights, data):
     """Generates class predictions given weights, and a test data matrix"""
     y_pred = np.dot(data, weights)
-    y_pred[np.where(y_pred <= 0)] = 0
+    y_pred[np.where(y_pred <= 0)] = -1
     y_pred[np.where(y_pred > 0)] = 1
     
     return y_pred
